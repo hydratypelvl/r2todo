9939d3519fbd2a40bf8a62d17c6c3477361535b7
hydratypelvl <rihards.adm@gmail.com>
2025-03-12 12:16:21 +0200

    Build fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
 "use client"
 
 import { LoginForm } from "@/components/login-form"
-import { useEffect } from "react"
 
 export default function Home() {
-    // <div className="m-4">Jautājumi pie Ritvara drauga...</div>
-
-    
-//   useEffect(() => {
-//     // window.location.href = 'https://r1riepas.lv/';
-//   }, []);
 
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
@@ -19,13 +12,3 @@ export default function Home() {
     </div>
   )
 }
-function redirectWithMessage() {
-    setTimeout(() => {
-        window.location.href = 'https://r1riepas.lv/';
-      }, 2000);
-    
-    return (
-        <div className="m-4">Jautājumi pie Ritvara drauga...</div>
-    )
-}
-     
